b47903ee84a98e4d16060d4c8593d644a9ca9fcd
armaber <armaber@github.com>
2024-12-23 23:08:03 +0200

    Add script to detect Native Hot Plug presence in the platform and OS.

---
+++ NativeHotPlugSupport/Detect_OSC.js
@@ -0,0 +1,103 @@
+﻿"use strict";
+
+function toString(s)
+{
+    if (typeof s == "string") {
+        return s;
+    }
+    var m = ""
+    for (var i of s) {
+        m += i + "\n";
+    }
+    return m;
+}
+
+function reloadSymbols(ctl)
+{
+    var output = ctl.ExecuteCommand(".sympath");
+
+    if (output.match("https://msdl.microsoft.com/download/symbols")) {
+        return;
+    }
+    ctl.ExecuteCommand(".sympath+ srv*https://msdl.microsoft.com/download/symbols");
+    ctl.ExecuteCommand(".reload");
+}
+
+function invokeScript()
+{
+    var ctl = host.namespace.Debugger.Utility.Control;
+    var dout, output, kinterrupt, first, devobj, gsiv, devext, hid;
+    var CTRL = 0;
+    var arbiter = new Map();
+
+    reloadSymbols(ctl);
+
+    var output = ctl.ExecuteCommand("!amli find _OSC");
+
+    for (var line of output) {
+        if (line.match("\\_SB\..{4}\._OSC")) {
+            var pci0 = line.replace("\._OSC","");
+ 
+            hid = ctl.ExecuteCommand("!amli dns /v " + pci0 + "._HID");
+            //EISAID("PNP0A08")
+            if (hid[2] != "Integer(_HID:Value=0x00000000080ad041[134926401])") {
+                continue;
+            }
+
+            line = "!amli dns /v " + line;
+            dout = ctl.ExecuteCommand(line);
+            var disassembly = "!amli u " + dout[2].split("CodeBuff=")[1].split(",")[0];
+            dout = ctl.ExecuteCommand(disassembly);
+            dout = toString(dout);
+            host.diagnostics.debugLog(dout);
+            if (dout.match(/ NHPG\(\)/g)) {
+                dout = ctl.ExecuteCommand("!amli dns /v " + pci0 + ".NHPG");
+                if (dout[0].match("AMLI_DBGERR:")) {
+                    dout = ctl.ExecuteCommand("!amli dns /v \\NHPG");
+                }
+                disassembly = "!amli u " + dout[2].split("CodeBuff=")[1].split(",")[0];
+                dout = ctl.ExecuteCommand(disassembly);
+                dout = toString(dout);
+                host.diagnostics.debugLog(dout);
+            }
+ 
+            disassembly = "!amli dns /v " + pci0 + ".SUPP";
+            dout = ctl.ExecuteCommand(disassembly);
+            host.diagnostics.debugLog(dout[2] + "\n");
+
+            disassembly = "!amli dns /v " + pci0 + ".CTRL";
+            dout = ctl.ExecuteCommand(disassembly);
+            CTRL = parseInt(dout[2].split("\[")[1].split("\]")[0], 10);
+            host.diagnostics.debugLog(dout[2] + "\n");
+
+            break;
+        }
+    }
+    if (CTRL & 1 == 0) {
+        return;
+    }
+    output = ctl.ExecuteCommand("!arbiter 4");
+    for (var line of output) {
+        if (line.match("\(pci\)")) {
+            first = "0x" + line.trim().split(" ")[0].substring(8);
+            devobj = "0x" + line.trim().split(/\b/)[4];
+            arbiter.set(first, devobj);
+        }
+    }
+    output = ctl.ExecuteCommand("!idt");
+    for (var line of output) {
+        if (line.match("pci!(ExpressRootPortMessageRoutine|ExpressDownstreamSwitchPortInterruptRoutine)")) {
+            line = line.replace(/\s+/g, " ").replace(/\: [0-9a-f]{16} /, ": ");
+            host.diagnostics.debugLog("\n" + line + "\n");
+            kinterrupt = (line.split("KINTERRUPT ")[1].split("\)")[0]);
+            dout = ctl.ExecuteCommand("dx ((nt!_KINTERRUPT *)0x" + kinterrupt + ")->ConnectionData->Vectors[0].ControllerInput.Gsiv");
+            gsiv = dout[0].split("\: ")[1].split(" ")[0];
+            dout = ctl.ExecuteCommand("dx ((nt!_DEVICE_OBJECT *)" + arbiter.get(gsiv) + ")->DeviceExtension");
+            devext = dout[0].split("\: ")[1].split(" ")[0];
+            dout = ctl.ExecuteCommand("!devext " + devext);
+            host.diagnostics.debugLog(dout[0] + "\n" + dout[1] + "\n" + dout[3] + "\n" + dout[4] + "\n");
+        }
+    }
+    output = ctl.ExecuteCommand("!sysinfo cpuinfo");
+    host.diagnostics.debugLog(output[4] + "\n" + output[5] + "\n");
+}
---
+++ NativeHotPlugSupport/NativeHotPlugSupport.ps1
@@ -0,0 +1,45 @@
+<#
+.SYNOPSIS
+Show native PCIe hot plug ACPI methods, ISRs
+
+.DESCRIPTION
+Use the script to process either the .DMP memory file or show the in-band kernel support
+for PCIe hot plug.
+
+CPU model, ACPI _OSC and CTL, IDT entries that manage hot plug with corresponding PDOs are
+part of listing.
+
+.PARAMETER Path
+Specify the path to a memory file. It must end in .dmp.
+
+.PARAMETER LocalKD
+Use local kernel connection, within an elevated prompt. Provision the OS with
+    bcdedit.exe /debug on
+    bcdedit.exe /dbgsettings local
+and disable SecureBoot in the firmware menu = BIOS.
+
+.LINK
+https://github.com/armaber/articles/blob/main/PCIeHP/notes.md
+#>
+[CmdletBindig(DefaultParameterSetName = "DMP")]
+param(
+    [Parameter(ParameterSetName = "DMP")]
+    [String]$Path,
+    [Parameter(ParameterSetName = "LKD")]
+    [Switch]$LocalKD
+);
+
+if ($Path -notlike "*.dmp") {
+    throw "The path must be a memory file";
+}
+
+$kd = "${env:ProgramFiles(x86)}\Windows Kits\10\Debuggers\x64\kd.exe";
+$st = ".load jsprovider.dll; .scriptrun ""$PSScriptRoot\Detect_OSC.js""; q";
+if ($PSCmdlet.ParameterSetName -eq "DMP") {
+    $output = & $kd -c $st -kqm -z $Path;
+} else {
+    $output = & $kd -c $st -kqm -kl;
+}
+$output = $output -join "`n";
+$parse = ([regex]::Matches($output, "JavaScript script successfully loaded.+(.*`n)+quit:").Value -split "`n");
+$parse[1..($parse.Count-2)];
